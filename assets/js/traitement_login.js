cbf43756fa923806aad268ec4932b710caa99210
Seergio1 <sergiorajaohariniaina@gmail.com>
2023-07-11 02:13:54 +0200

    version 3 Sergio

--- assets/js/traitement_login.js
+++ assets/js/traitement_login.js
@@ -17,7 +17,23 @@ function getXHR() {
 }
 function login_front_office(){
     var login_front_office = document.getElementById('demo-form_1');
-    var formData = new FormData(login_front_office);
+    var formData = new FormData();
+	formData.append('objectif',value_objectif);
+	formData.append('genre',value_genre);
+	formData.append('age',value_age);
+	formData.append('poids_actuel',value_poids_actuel);
+	formData.append('taille',value_taille);
+	formData.append('restriction_alimentaire',tab_restriction_alimentaire);
+	formData.append('maladie',tab_maladie);
+	formData.append('frequence_sport',value_frequence_sport);
+	formData.append('nbr_repas',value_nbr_repas_jour);
+	formData.append('argent',value_argent);
+	formData.append('rythme',value_rythme);
+	formData.append('poids_ideal',value_poids_ideal);
+	formData.append('name',value_name);
+	formData.append('email',value_email);
+	formData.append('password',value_password);
+	formData.append('dtn',value_dateN);
 	let xhr = getXHR();
 	xhr.onreadystatechange = function () {
 
@@ -122,18 +138,29 @@ function login_back_office(){
 var btn_log_back_office = document.getElementById('btn_log_back_office');
 var btn_log_front_office = document.getElementById('btn_log_front_office');
 var btn_inscri_front_office = document.getElementById('btn_inscri_front_office');
-if(btn_log_back_office!=null){
-    btn_log_back_office.addEventListener('click',()=>{
-        login_back_office();
-    });
-}
-if(btn_log_front_office!=null){
-    btn_log_front_office.addEventListener('click',()=>{
+
+
+
+btn_inscri_front_office.addEventListener('click',()=>{
+		console.log("bouton");
+		getLogin();
+		console.log(value_age);
+		console.log(value_argent);
+		console.log(value_frequence_sport);
+		console.log(value_nbr_repas_jour);
+		console.log(tab_restriction_alimentaire);
+		console.log(value_taille);
+		console.log(value_rythme);
+		console.log(value_genre);
+		console.log(tab_restriction_alimentaire);
+		console.log(value_poids_ideal);
+		console.log(value_objectif);
+		console.log(value_poids_actuel);
+		console.log(value_name);
+		console.log(value_email);
+		console.log(value_password);
+		console.log(value_dateN);
         login_front_office();
     });
-}
-if(btn_inscri_front_office!=null){
-    btn_inscri_front_office.addEventListener('click',()=>{
-        inscription_front_office();
-    });
-}
+
+
